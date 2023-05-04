24d44e048ddf4f849e5eebec9ebf1029a3970336
saad raja <shraja7@gmail.com>
2023-05-04 10:00:46 -0500

    add product details page

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -9,10 +10,16 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
         <Card.Text as="div">
           <div className="my-3">
             {product.rating} from {product.numReviews} reviews
