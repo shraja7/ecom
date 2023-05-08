eaf7456fb86706297b52012c823bd3a2ba03bfba
saad raja <shraja7@gmail.com>
2023-05-08 12:39:50 -0500

    implement async handler to productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
 const router = express.Router();
 import products from "../data/products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.json(products);
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+  })
+);
 
 export default router;
