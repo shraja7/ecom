873ac061eb18cbe34cdba79f4a958d36d071c473
saad raja <shraja7@gmail.com>
2023-05-08 14:17:37 -0500

    refactor product routes into controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,11 @@
 import express from "express";
 const router = express.Router();
-import Product from "../models/productModel.js";
-import asyncHandler from "../middleware/asyncHandler.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    //if no product is found, throw an error
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404).json({ message: "Product not found" });
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); //connect to database
@@ -16,6 +17,10 @@ app.get("/", (req, res) => {
 
 // use the productRoutes middleware
 app.use("/api/products", productRoutes);
+
+//middleware for handling 404 errors
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
