091a12c8a23c3edd519cf3097dfe49da4eecb858
saad raja <shraja7@gmail.com>
2023-05-11 16:14:41 -0500

    implement user routes and add to server

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+//bring contorller functions from userController
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";
+//import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/login", authUser);
+router.post("/logout").get(logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,8 @@ app.get("/", (req, res) => {
 
 // use the productRoutes middleware
 app.use("/api/products", productRoutes);
+// use the userRoutes middleware
+app.use("/api/users", userRoutes);
 
 //middleware for handling 404 errors
 app.use(notFound);
