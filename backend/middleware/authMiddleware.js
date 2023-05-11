9d646d46263170385b0e513c5c87e4a874d426fb
saad raja <shraja7@gmail.com>
2023-05-11 17:37:40 -0500

    implement protect middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler"; //import asyncHandler
+import User from "../models/userModel.js";
+
+//have two routes, protected and admin
+
+//protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  //read the jwt from the cookie
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      //verify the token
+      //an object that contains the user id field
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //add user to the request object
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
