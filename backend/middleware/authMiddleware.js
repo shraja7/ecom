42d79746c58cd6dd01d3b69bfcc885e38639b879
saad raja <shraja7@gmail.com>
2023-05-11 17:38:32 -0500

    implement admin middlware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,3 +29,16 @@ const protect = asyncHandler(async (req, res, next) => {
     throw new Error("Not authorized, no token");
   }
 });
+
+//admin middleware
+const admin = (req, res, next) => {
+  //check if user is admin
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, admin };
