2fa04d33e6fcc791c224dd331884c298e00ccee0
saad raja <shraja7@gmail.com>
2023-05-11 17:29:25 -0500

    implement jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -9,6 +10,16 @@ const authUser = asyncHandler(async (req, res) => {
   //check if user exists
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    //set jwt as httpOnly cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60 * 24 * 30, //30 days
+    });
     res.json({
       _id: user._id,
       name: user.name,
