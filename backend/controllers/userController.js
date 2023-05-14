f5dd3a7ec75970c07ae5452dc79d2924d657237d
saad raja <shraja7@gmail.com>
2023-05-13 20:13:03 -0500

    implement getUserProfile user controller with real data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,7 +76,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.userId);
+  //check to see if user exists
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //update user profile
