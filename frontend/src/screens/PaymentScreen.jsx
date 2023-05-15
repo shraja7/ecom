9d81586e02df29fa4c4379e48a9258aa78df29ba
saad raja <shraja7@gmail.com>
2023-05-15 11:14:30 -0500

    add PaymentScreen

---
+++ frontend/src/screens/PaymentScreen.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+const PaymentScreen = () => {
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Stripe"
+              id="Stripe"
+              name="paymentMethod"
+              value="Stripe"
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
